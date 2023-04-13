bdf4aa501307b259202e56d7d035f91147382980
carlos <cigarcia@ymail.com>
2023-04-13 14:01:00 +0200

    creación de vistas y urls

--- ProyectoWeb/urls.py
+++ ProyectoWeb/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #Abajo se enlazan los archivos url de cada aplicacion
+    path("", include("proyectoWebApp.urls")),  
+    
 ]
--- proyectoWebApp/views.py
+++ proyectoWebApp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("Home")
+    
+    # return render(request, "proyectoWebApp\home")
+
+def servicios(request):
+    return HttpResponse("aqui son los servicios")
+#     return render(request, "proyectoWebApp\servicios")
+
+def tienda(request):
+    return HttpResponse("esto es la tienda")
+#     return render(request, "proyectoWebApp\tienda")
+
+def blog(request):
+    return HttpResponse("esto es el blog")
+#     return render(request, "proyectoWebApp\blog")
+
+def contacto(request):
+    return HttpResponse("esto es el contacto")
+#     return render(request, "proyectoWebApp\contacto")
