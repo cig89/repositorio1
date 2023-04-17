da339d6a541d2134470ec064c4b5bc5539511b99
carlos <cigarcia@ymail.com>
2023-04-17 12:21:38 +0200

    serviciosApp - configuracion del panel de administracion

--- serviciosApp/admin.py
+++ serviciosApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Servicio
 
-# Register your models here.
+#La clase creada a continuación es opcinal, simplemene nos permite personalizar como se ve la tabla y porque cambos buscar y filtrar
+class ServicioAdmin(admin.ModelAdmin):
+    list_display  =  ['titulo', 'contenido', 'imagen','created', 'updated']  # esto son los campos que mostrará la tabla en el panel de administracion
+    search_fields =  ['titulo', 'contenido','created', 'updated']             # esto son los campos para buscar la información
+    list_filter   =  ['titulo', 'contenido']                                  #son los filtros que aparecen 
+    
+    
+admin.site.register(Servicio, ServicioAdmin)    #esto es obligatorio, ya que añade la tabla al panel de administración
--- serviciosApp/models.py
+++ serviciosApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Servicio(models.Model):
+    titulo = models.CharField(max_length=50)
+    contenido = models.CharField(max_length=50)
+    imagen = models.ImageField()
+    """se crean los campos created & updated para filtrar en la tabla"""
+    created = models.DateTimeField(auto_now_add =True) #agrega la fecha automa cuando se cree el registro
+    updated = models.DateTimeField(auto_now_add =True) #agrega la fecha automa cuando se actualice el registro
+
+    class Meta():
+        """" la clase meta  permite  cambiar a singular y plural cuando nos aparezca la tabla en el panel de administración."""
+        verbose_name = "Servicio"
+        verbose_name_plural ="Servicios"
+        
+    def __str__(self) -> str:
+        return self.titulo
+    
+    
