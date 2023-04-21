be150225c4e176fabc40d2b9b5cc618b69315d7b
carlos <cigarcia@ymail.com>
2023-04-21 14:57:57 +0200

    shopApp-tablas en pAdmin

--- shopApp/admin.py
+++ shopApp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from shopApp.models import CategoriaProd,Producto
+
+""" PARA CATEGORIA"""
+#La clase creada a continuación es opcinal, simplemene nos permite personalizar como se ve la tabla y porque cambos buscar y filtrar
+class CategoriaProdAdmin(admin.ModelAdmin):
+    list_display  =  ['nombre','created', 'updated']  # esto son los campos que mostrará la tabla en el panel de administracion
+    search_fields =  ['nombre','created', 'updated']  # esto son los campos para buscar la información
+    list_filter   =  ['nombre']                       # son los filtros que aparecen 
+    
+    
+admin.site.register(CategoriaProd, CategoriaProdAdmin)    #esto es obligatorio, ya que añade la tabla al panel de administración
+
+
+""" PARA PRODUCTO"""
+#La clase creada a continuación es opcinal, simplemene nos permite personalizar como se ve la tabla y porque cambos buscar y filtrar
+class ProductoAdmin(admin.ModelAdmin):
+    list_display  =  ['nombre', 'precio', 'disponibilidad', 'created','updated']        # esto son los campos que mostrará la tabla en el panel de administracion
+    search_fields =  ['nombre', 'precio', 'disponibilidad','created', 'updated']        # esto son los campos para buscar la información
+    list_filter   =  ['nombre']                                           # son los filtros que aparecen 
+    
+    
+admin.site.register(Producto, ProductoAdmin)    #esto es obligatorio, ya que añade la tabla al panel de administración
--- shopApp/models.py
+++ shopApp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class CategoriaProd(models.Model):
+    nombre =  models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = "CategoriaProd"
+        verbose_name_plural = "CategoriasProd"
+        
+        def __str__(self):
+            return self.nombre
+    
+class Producto(models.Model):
+    nombre =         models.CharField(max_length=200)
+    categorias =     models.ForeignKey(CategoriaProd, on_delete=models.CASCADE)
+    imagen =         models.ImageField(upload_to="shopApp")
+    precio =         models.FloatField()
+    disponibilidad = models.BooleanField(default=True)
+    created =        models.DateTimeField(auto_now_add=True)
+    updated =        models.DateTimeField(auto_now_add=True)
+    
+    
+    class Meta: 
+        verbose_name = "Producto"
+        verbose_name_plural ="Productos"
+        
+        def __str__(self):
+            return self.nombre
+    
+    
+    
