44e1613fd5e713cd147842a8579e846f8998ba5b
carlos <cigarcia@ymail.com>
2023-05-08 13:20:06 +0200

    botones de inicio y registro

--- loginApp/urls.py
+++ loginApp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #el 1er arg es la palabra para la url. El 2arg es la ruta a la view. El 3arg es el nombre que le damos a la ruta entera.
     
     #path("", VistaRegistro.as_view(), name ="Autenticacion"),
-    path("", views.get, name ="mostrar"),
-    path("post/", views.post, name ="Recibir"),
+    path("", views.get, name ="FormularioRegistro"),
+    path("post/", views.post, name ="RecibirFormularioRegistro"),
     
 ]
