8f7b5febd49de1f214886377e1058dd6af444c2f
carlos <cigarcia@ymail.com>
2023-05-10 13:48:16 +0200

    carroApp - on/off carro si no se esta logeado

--- carroApp/context_processors.py
+++ carroApp/context_processors.py
@@ -4,7 +4,6 @@ from .views import Carro
 def importe_total_carro(request):
     total =0  
     carro = Carro(request)
-    
     for key,value in request.session["carro"].items():
         total = total + float(value["precio"])
             
--- proyectoWebApp/views.py
+++ proyectoWebApp/views.py
@@ -1,6 +1,8 @@
 
 from django.shortcuts import render
 
+from carroApp.views import Carro
+
 
 def home(request):  
     return render(request, "home.html")
